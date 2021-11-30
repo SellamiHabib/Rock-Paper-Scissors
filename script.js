c07d3e2161daa35f3d1ef6f22ca06ff0fc14e416
Habib Sellami <sellami.habib@hotmail.com>
2021-11-30 19:53:46 +0100

    Added the the round results

--- script.js
+++ script.js
@@ -13,21 +13,51 @@ function playRound(playerSelection )
         
         let pSelection = playerSelection.toLowerCase();
         if ( pSelection == computerSelection )
-            return "Draw";
+            return 0
         if( (pSelection == "rock") && (computerSelection == "scissors") 
             || (pSelection == "scissors") && (computerSelection == "paper") 
             || (pSelection == "paper") && (computerSelection == "rock") 
             )
-            return "You win";
+            return 1;
         else 
-            return "You lose";
+            return -1;
     }
+function updateResult (id){
 
+    let result = document.getElementById("rslt");
+
+    if(id == 0){
+        result.innerText = "Draw!"
+    }
+    else if(id == 1) {
+        let winnerhtml = document.getElementById("p");
+        let score = parseInt(winnerhtml.innerText,10);
+        ++score;
+        console.log(winnerhtml.innerText);
+        winnerhtml.innerText = score;
+        result.innerText = "You win!";
+        if (score >=5)
+            window.alert("You won the game!");
+
+    }
+    else {
+
+        let winnerhtml = document.getElementById("c");
+        let score = parseInt(winnerhtml.innerText,10);
+        ++score;
+        console.log(winnerhtml.innerText);
+        winnerhtml.innerText = score;
+        result.innerText = "You lose!";
+        if (score >=5)
+            window.alert("You lost the game!");
+    }
+    
+    
+}
 const btns = document.querySelectorAll("button");
 btns.forEach( (btn) => {
 
     btn.addEventListener("click", () => {
-        
-        console.log(playRound(btn.id));
+        updateResult(playRound(btn.id));
     });
 } );
